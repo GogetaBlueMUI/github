51a7d4226dd92555c6dd796b0df5ce74d2476ec0
IQ-Fero <ibadullahqureshi55@gmail.com>
2024-07-19 05:49:42 -0700

    goal

---
+++ goal.py
@@ -0,0 +1,11 @@
+name = "Ibadullah Qureshi doing BS in AI in PAF-IAST"
+goal = "establish a software house and elevate it to a national and then international level"
+importance = "This goal is crucial as it represents the peak of my passion for technology and entrepreneurship, offering opportunities to innovate and make a global impact."
+plans = "To achieve this, I plan to build a skilled team, invest in advanced and modern technologies, and focus on high-quality service delivery."
+challenges = "The main challenges include intense competition and the need for continuous adaptation and typical family pressure. I plan to overcome these by staying updated with industry trends and adapting habit of continuous learning."
+
+print(f"My name is {name}.")
+print(f"My primary goal in life is to {goal}.")
+print(f"{importance}")
+print(f"My plans to achieve this include: {plans}.")
+print(f"The challenges I anticipate are: {challenges}")
